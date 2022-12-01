40099f6ccf772f2f7acb5a547f368be4c277c980
Theodore Kelechukwu Onyejiaku <theodoreonyejiaku@gmail.com>
2022-12-01 23:43:08 +0100

    refactor app.js

--- app.js
+++ app.js
@@ -11,16 +11,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json()) // To parse the incoming requests with JSON payloads
 app.use(cors())
 
-app.get("*", async (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend', 'build', "index.html"))
-})
-
 app.post("/email", async (req, res) => {
     const { error } = await sendEmail(req.body.email, req.body.fullName, req.body.message)
     if (error) {
         return res.status(500).json("something went wrong")
     }
     return res.status(200).json("Email sent! Please check your inbox")
+})
+
+app.get("*", async (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'build', "index.html"))
 })
 
 app.listen(PORT, () => {
